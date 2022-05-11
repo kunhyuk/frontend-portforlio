557bea688b2e9d8d32ed77fc0b9b9f46f01d80b3
Kunhyuk <klee762@gatech.edu>
2022-05-11 14:10:51 -0400

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 // IMPORT COMPONENTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // IMPORT PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,12 +11,12 @@ import Projects from "./pages/Projects";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "http://localhost:4000/";
+  const URL = "http://localhost:3000/";
 
   return (
     <div className="App">
       <Header />
-      <Switch>
+      <Routes>
         <Route exact path="/">
           <Home />
         </Route>
@@ -26,7 +26,7 @@ function App() {
         <Route path="/about">
           <About URL={URL} />
         </Route>
-      </Switch>
+      </Routes>
       <Footer />
     </div>
   );
